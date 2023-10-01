af2df4957736d24eb08387f325b77e4a0f08a958
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2023-10-01 20:53:16 +0200

    Update LinkToCrud by role

--- migrations/Version20230928132956.php
+++ migrations/Version20230928132956.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230926135016 extends AbstractMigration
+final class Version20230928132956 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,15 @@ final class Version20230926135016 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "user" ADD is_verified BOOLEAN NOT NULL');
+        $this->addSql('CREATE SEQUENCE test_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE test (id INT NOT NULL, test VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE "user" DROP is_verified');
+        $this->addSql('DROP SEQUENCE test_id_seq CASCADE');
+        $this->addSql('DROP TABLE test');
     }
 }
